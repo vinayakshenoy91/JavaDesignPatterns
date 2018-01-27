cf8aee1f3d6a69d44923242d10bf82ebec2e3682
vinayak shenoy k <vinayakshenoyk@INMACMCC1G.local>
2018-01-27 12:27:37 +0530

    [vs]Commited singleton pattern

---
+++ CreationalPattern/DesignPatternDemo.java
@@ -0,0 +1,7 @@
+
+public class DesignPatternDemo {
+	public static void main(String[] args) {
+		System.out.println(SingletonPattern.getInstance());
+		System.out.println(SingletonPattern.getInstance());
+	}
+}
---
+++ CreationalPattern/SingletonPattern.java
@@ -0,0 +1,21 @@
+
+//Thread Safe & Lazy Loaded singleton pattern
+public class SingletonPattern {
+
+	public static SingletonPattern instance = null;
+
+	private SingletonPattern() {
+
+	}
+
+	public static SingletonPattern getInstance() {
+		if (instance == null) {
+			synchronized (SingletonPattern.class) {
+				if (instance == null) {
+					instance = new SingletonPattern();
+				}
+			}
+		}
+		return instance;
+	}
+}
